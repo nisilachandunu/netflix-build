1d192fb9294a17c8a579aff22422edbe31f3d4f5
Nisila Chandunu <85236657+nisilachandunu@users.noreply.github.com>
2023-07-02 20:57:38 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBRSwMp_rDpuqfd7WNN39T7i7QPhqsXz-4",
+  authDomain: "netflix-clone-6bc5c.firebaseapp.com",
+  projectId: "netflix-clone-6bc5c",
+  storageBucket: "netflix-clone-6bc5c.appspot.com",
+  messagingSenderId: "1097755313957",
+  appId: "1:1097755313957:web:688947227ac4900c5d7ccc",
+};
