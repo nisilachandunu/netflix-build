8e873c02611757616dc9c0a9eca3803bf1196814
Nisila Chandunu <85236657+nisilachandunu@users.noreply.github.com>
2023-07-03 17:28:30 +0530

    Second Commit

--- src/Nav.js
+++ src/Nav.js
@@ -1,8 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Nav.css";
 
 function Nav() {
-  return <div className="nav"></div>;
+  const [show, handleShow] = useState(false);
+
+  const transitionNavBar = () => {
+    if (window.scrollY > 100) {
+      handleShow(true);
+    } else {
+      handleShow(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", transitionNavBar);
+    return () => window.removeEventListener("scroll", transitionNavBar);
+  }, []);
+  return (
+    <div className={`nav ${show && "nav_black"}`}>
+      <div className="nav_contents">
+        <img
+          className="nav_logo"
+          src="https://www.edigitalagency.com.au/wp-content/uploads/Netflix-logo-red-black-png.png"
+          alt="netflix-logo"
+        />
+
+        <img
+          className="nav_avatar"
+          src="https://www.pngall.com/wp-content/uploads/12/Avatar-Profile-PNG-Free-Image.png"
+          alt="avatar"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Nav;
